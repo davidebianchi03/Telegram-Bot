4808644741572a79b286e32f36414131339bcef0
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-02-28 17:06:59 +0100

    Created class 'Telegram', created method for getting updates, created simple main class

--- Telegram.API/src/Telegram/API/Telegram.java
+++ Telegram.API/src/Telegram/API/Telegram.java
@@ -1,20 +1,35 @@
 package Telegram.API;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  *
  * @author Davide
- * Classe principale che gestisce la comu
+ * Class used for making requests
  */
 public class Telegram {
     
     private static Telegram instance = null;
     private String apiKey; 
+    private int lastUpdateId;
+    private String basePath;
     
     private Telegram(String apiKey){
         this.apiKey = apiKey;
+        lastUpdateId = 0;
+        basePath = "https://api.telegram.org/bot" + apiKey;
     }
     
-    //metodo da usare per ottenere l'instanza
+    //method for getting the instance
     public static Telegram Init(String apiKey){
         
         if(instance == null){
@@ -27,6 +42,50 @@ public class Telegram {
         return instance;
     }
     
-    //
-    
+    //method for getting the list of new messages
+    public List<Message> GetUpdates(){
+        List<Message> messages = new LinkedList<>();
+        
+        //load the list of available messages
+        String urlPath = basePath + "/getUpdates?offset=" + Integer.toString(lastUpdateId + 1);
+        
+        try {
+            URL url = new URL(urlPath);
+            Scanner urlScanner = new Scanner(url.openStream());
+            urlScanner.useDelimiter("/u000a");
+            String response = urlScanner.next();
+      
+            //messages list parsing
+            JSONObject jsonObj = new JSONObject(response);
+            if(jsonObj.has("ok")){
+                if(jsonObj.getBoolean("ok")){
+                    if(jsonObj.has("result")){
+                        JSONArray resultsArray = jsonObj.getJSONArray("result");
+                        
+                        for(int i = 0;i<resultsArray.length();i++){
+                            JSONObject resultObj = resultsArray.getJSONObject(i);
+                            //load message
+                            if(resultObj.has("message")){
+                                messages.add(Message.LoadFromJson(resultObj.getJSONObject("message")));
+                            }
+                            //load update_id
+                            if(resultObj.has("update_id")){
+                                lastUpdateId = resultObj.getInt("update_id");
+                            }
+                        }
+                        return messages;
+                    }
+                }
+            }
+            return null;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Telegram.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (IOException ex) {
+            Logger.getLogger(Telegram.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
+        
+    }
 }
--- jbot/src/jbot/Jbot.java
+++ jbot/src/jbot/Jbot.java
@@ -1,6 +1,9 @@
 package jbot;
 
 import Telegram.API.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,9 +15,24 @@ public class Jbot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        
+
+        Telegram telegram = Telegram.Init("");
+        List<Message> updatesList = null;
+        while (true) {
+            updatesList = telegram.GetUpdates();
+            for (int i = 0; i < updatesList.size(); i++) {
+                Message currentMessage = updatesList.get(i);
+                String text = currentMessage.getText();
+                User sender = currentMessage.getFrom();
+                System.out.println("Sender: " + sender.getFirst_name() + " " + sender.getLast_name() + " - Message: " + text);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Jbot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
     }
-    
+
 }
