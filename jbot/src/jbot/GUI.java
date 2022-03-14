d6c57cd94dcaf5727c060d5644371fa1adfb64d2
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2022-03-14 17:24:57 +0100

    Selezione posizione scrivendo il nome del luogo

--- jbot/src/jbot/GUI.java
+++ jbot/src/jbot/GUI.java
@@ -1,17 +1,26 @@
 package jbot;
 
 import GeoUtilis.Coordinate;
+import GeoUtilis.Geocoding;
+import GeoUtilis.Place;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -19,9 +28,9 @@ import javax.swing.JTextField;
  */
 public class GUI extends JFrame {
 
-    public JTextField txt_latitude;
-    public JTextField txt_longitude;
+    public JTextField txt_citta;
     public JTextArea txt_pubblicita;
+    public JTextField txt_raggio;
     public ADVList advList;
 
     public GUI() {
@@ -35,24 +44,24 @@ public class GUI extends JFrame {
 
     public void DrawGUI() {
         //elementi per l'inserimento della latitudine
-        JLabel lbl_lat = new JLabel("Latitudine");
-        lbl_lat.setBounds(300, 100, 100, 25);
-        lbl_lat.setFont(new Font("Verdana", Font.PLAIN, 13));
-        this.add(lbl_lat);
-        txt_latitude = new JTextField("45.0");
-        txt_latitude.setBounds(400, 100, 250, 25);
-        txt_latitude.setFont(new Font("Verdana", Font.PLAIN, 13));
-        this.add(txt_latitude);
+        JLabel lbl_citta = new JLabel("Città");
+        lbl_citta.setBounds(300, 100, 100, 25);
+        lbl_citta.setFont(new Font("Verdana", Font.PLAIN, 13));
+        this.add(lbl_citta);
+        txt_citta = new JTextField("");
+        txt_citta.setBounds(400, 100, 250, 25);
+        txt_citta.setFont(new Font("Verdana", Font.PLAIN, 13));
+        this.add(txt_citta);
 
         //elementi per l'inserimento della longitudine
-        JLabel lbl_lon = new JLabel("Longitudine");
-        lbl_lon.setBounds(300, 140, 100, 25);
-        lbl_lon.setFont(new Font("Verdana", Font.PLAIN, 13));
-        this.add(lbl_lon);
-        txt_longitude = new JTextField("9.0");
-        txt_longitude.setBounds(400, 140, 250, 25);
-        txt_longitude.setFont(new Font("Verdana", Font.PLAIN, 13));
-        this.add(txt_longitude);
+        JLabel lbl_raggio = new JLabel("Raggio (in metri)");
+        lbl_raggio.setBounds(275, 140, 250, 25);
+        lbl_raggio.setFont(new Font("Verdana", Font.PLAIN, 13));
+        this.add(lbl_raggio);
+        txt_raggio = new JTextField("1000");
+        txt_raggio.setBounds(400, 140, 250, 25);
+        txt_raggio.setFont(new Font("Verdana", Font.PLAIN, 13));
+        this.add(txt_raggio);
 
         //elementi per l'inserimento della descrizione della pubblicità
         JLabel lbl_description = new JLabel("Descrizione");
@@ -65,23 +74,47 @@ public class GUI extends JFrame {
         txt_pubblicita.setWrapStyleWord(true);
         txt_pubblicita.setFont(new Font("Verdana", Font.PLAIN, 13));
         this.add(txt_pubblicita);
-        
+
         //pulsante utilizzato per confermare l'inserimento
         JButton submit_btn = new JButton("Aggiungi");
         submit_btn.setBackground(new Color(134, 194, 247));
         submit_btn.setFont(new Font("Verdana", Font.PLAIN, 13));
         submit_btn.setBounds(450, 300, 100, 40);
-        submit_btn.addActionListener(new ActionListener(){
+        submit_btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //controllo se sono stati aggiunti tutti i campi e in caso affermativo salvo la pubblicità nel file
-                if(txt_pubblicita.getText() != "" && IsDouble(txt_latitude.getText()) && IsDouble(txt_longitude.getText())){
-                    double lat = Double.parseDouble(txt_latitude.getText());
-                    double lon = Double.parseDouble(txt_longitude.getText());
-                    SendADV sa = new SendADV(new Coordinate(lat, lon), txt_pubblicita.getText());
-                    sa.start();
+                if (txt_pubblicita.getText() != "" && IsRadiusValid(txt_raggio.getText()) && txt_citta.getText() != "") {
+                    try {
+                        List<Place> placeList = Geocoding.SearchPlace(txt_citta.getText());
+
+                        if (placeList.size() > 0) {
+                            SendADV sa = new SendADV(placeList.get(0).getCoordinate(), txt_pubblicita.getText(), Integer.parseInt(txt_raggio.getText()));
+                            sa.start();
+                            JOptionPane.showMessageDialog(txt_citta.getParent(), "Messaggi inviati correttamente");
+                            txt_pubblicita.setText("");
+                            txt_raggio.setText("");
+                            txt_citta.setText("");
+                        } else {
+                            JOptionPane.showMessageDialog(txt_citta.getParent(), "Non esistono luoghi con questo nome");
+                        }
+
+                    } catch (ParserConfigurationException ex) {
+                        Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+                        JOptionPane.showMessageDialog(txt_citta.getParent(), "Errore nell'invio del messaggio");
+                    } catch (SAXException ex) {
+                        Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+                        JOptionPane.showMessageDialog(txt_citta.getParent(), "Errore nell'invio del messaggio");
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+                        JOptionPane.showMessageDialog(txt_citta.getParent(), "Errore nell'invio del messaggio");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(txt_citta.getParent(), "Non tutti i campi sono stati riempiti in modo corretto");
                 }
             }
+
         });
         this.add(submit_btn);
     }
@@ -93,14 +126,16 @@ public class GUI extends JFrame {
     }
 
     //funzione per controllare se una stringa è un numero double oppure no
-    private boolean IsDouble(String text){
-        try{
-            Double.parseDouble(text);
+    private boolean IsRadiusValid(String text) {
+        try {
+            int r = Integer.parseInt(text);
+            if (r <= 0) {
+                return false;
+            }
             return true;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
-    
+
 }
--- jbot/src/jbot/SendADV.java
+++ jbot/src/jbot/SendADV.java
@@ -12,30 +12,32 @@ import TelegramAPI.Telegram;
  *
  * @author Davide
  */
-public class SendADV extends Thread{
-    
+public class SendADV extends Thread {
+
     private Coordinate coordinate;
     private String message;
-    
-    public SendADV(Coordinate coordinate, String message){
+    private int raggio;
+
+    public SendADV(Coordinate coordinate, String message, int raggio) {
         this.coordinate = coordinate;
         this.message = message;
+        this.raggio = raggio;
     }
 
     @Override
     public void run() {
         UserList userList = UserList.GetInstance();
         Telegram t = Telegram.Init("");
-        for(int i=0;i<userList.GetSize();i++){
+        for (int i = 0; i < userList.GetSize(); i++) {
             User u = userList.GetUserAt(i);
-            if(Distance.CalculateDistanceInMeters(u.getCoordinate(), coordinate)<5000){
-                t.SendMessage(u.getChat_id(), "<b>Ciao " + u.getName() + ", è presente la seguente offerta vicino a te:</b>\n" + message);
-                t.SendLocation(u.getChat_id(), coordinate.getLatitude(), coordinate.getLongitude());
-           }
+            if (Distance.CalculateDistanceInMeters(u.getCoordinate(), coordinate) < raggio) {
+                synchronized (SendADV.class) {
+                    t.SendMessage(u.getChat_id(), "<b>Ciao " + u.getName() + ", è presente la seguente offerta vicino a te:</b>\n" + message);
+                    t.SendLocation(u.getChat_id(), coordinate.getLatitude(), coordinate.getLongitude());
+                }
+            }
         }
-        
+
     }
-    
-    
-    
+
 }
